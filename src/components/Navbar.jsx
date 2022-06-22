6ac3cce91f49dc26b279341463dade2afc416540
shadik <shadikmahamudsohan@gmail.com>
2022-06-22 18:41:46 +0600

    navbar compleated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,6 +11,7 @@ import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
+    // Making the nav button
     <TooltipComponent content={title} position="BottomCenter">
         <button
             type="button"
@@ -25,13 +26,30 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
             {icon}
         </button>
     </TooltipComponent>
+    // End ---------------------------
 );
 
 const Navbar = () => {
-    const { activeMenu, setActiveMenu } = useStateContext();
-    const handleClick = (p) => {
-        alert(`${p} is clicked`);
-    };
+    const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize } = useStateContext();
+
+    // making the side bar responsive
+    useEffect(() => {
+        const handleResize = () => setScreenSize(window.innerWidth);
+        window.addEventListener('resize', handleResize);
+
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    useEffect(() => {
+        if (screenSize <= 900) {
+            setActiveMenu(false);
+        } else {
+            setActiveMenu(true);
+        }
+    }, [screenSize]);
+    // End --------------------------
     return (
         <div className='flex justify-between p-2 md:mx-6 relative'>
             <NavButton title="Menu" customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)} color="blue" icon={<AiOutlineMenu />} />
@@ -66,10 +84,18 @@ const Navbar = () => {
                             alt="profile" />
                         <p>
                             <span className='text-gray-400 text-14'>Hi,</span>{' '}
-                            <span className='text-gray-400 font-bold ml-3 text-14'>Michael</span>
+                            <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
                         </p>
+                        <MdKeyboardArrowDown
+                            className='text-gray-400 text-14' />
                     </div>
                 </TooltipComponent>
+                {/* Showing every data when clicked */}
+                {isClicked.cart && <Cart />}
+                {isClicked.chat && <Chat />}
+                {isClicked.notification && <Notification />}
+                {isClicked.userProfile && <UserProfile />}
+                {/* End ---------------------------- */}
             </div>
         </div>
     );
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -11,9 +11,21 @@ const initialState = {
 
 export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+    const [screenSize, setScreenSize] = useState(undefined);
+    const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
     return (
         <StatContext.Provider
-            value={{ activeMenu, setActiveMenu }}>
+            value={{
+                activeMenu,
+                setActiveMenu,
+                isClicked,
+                setIsClicked,
+                handleClick,
+                screenSize,
+                setScreenSize
+            }}>
             {children}
         </StatContext.Provider>
     );
